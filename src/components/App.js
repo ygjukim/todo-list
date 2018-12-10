4fdfcab92edf7a57b7a017128b5b4e0827815e7f
Young-ju, Kim <ygjukim@gmail.com>
2018-12-10 10:43:21 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PageTemplate from './PageTemplate';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+
+// const initialTodos = new Array(500).fill(0).map(
+//     (foo, index) => ({ id: index, text: `일정 ${index}`, done: false})
+// );
+
+class App extends Component {
+    state = {
+        input: '',
+        todos: [
+            {id: 0, text: '리액트 공부하기', done:true},
+            {id: 1, text: '컴포넌트 스타일링 해보기', done:false},
+        ]
+        // todos: initialTodos
+    }
+
+    id = 1
+    getId = () => {
+        return ++this.id;
+    }
+
+    handleInputChange = (e) => {
+        const { value } = e.target;
+        this.setState({
+            input: value
+        });
+    }
+
+    handleInsert = () => {
+        const {input, todos} = this.state;
+
+        const newTodo = {
+            text: input,
+            done: false,
+            id: this.getId()
+        };
+
+        this.setState({
+            todos: [...todos, newTodo],
+            input: ''
+        });
+    }
+
+    handleToggle = (id) => {
+        const { todos } = this.state;
+        const index = todos.findIndex(todo => todo.id === id);
+        const toggled = {
+            ...todos[index],
+            done: !todos[index].done
+        };
+
+        this.setState({
+            todos: [
+                ...todos.slice(0, index),
+                toggled,
+                ...todos.slice(index+1, todos.length)]
+        });
+    }
+
+    handleRemove = (id) => {
+        const { todos } = this.state;
+        const index = todos.findIndex(todo => todo.id === id);
+
+        this.setState({
+            todos: [
+                ...todos.slice(0, index),
+                ...todos.slice(index+1, todos.length)]
+        });
+    }
+
+    render () {
+        const { input, todos } = this.state;
+        const { handleInputChange, handleInsert, handleToggle, handleRemove } = this;
+
+        return (
+            <div>
+                <PageTemplate>
+                    <TodoInput value={input} onChange={handleInputChange} onInsert={handleInsert} />
+                    <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
+                </PageTemplate>
+            </div>
+        );
+    }
+}
+
+export default App;
